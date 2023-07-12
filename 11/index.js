1a580f00da8042c5243b60c7300c8d5ea8f8e91a
Talita <109548286+talitadsa@users.noreply.github.com>
2023-07-12 08:15:46 -0300

    Update index.js

--- 11/index.js
+++ 11/index.js
@@ -1,5 +1,5 @@
 function calculaVolume(r){
-    const volume = (4/3 * 3.14 * r**3).toFixed(2);
+    const volume = (4/3 * Math.PI * r**3).toFixed(2);
     console.log(`O volume de uma esfera de raio ${r} é de ${volume}.`);
 }
 

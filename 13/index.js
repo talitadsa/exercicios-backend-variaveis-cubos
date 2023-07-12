ef00b8fe449c141e8f6f5cacbbb6bb0351dfbc8d
Talita <109548286+talitadsa@users.noreply.github.com>
2023-07-12 08:16:56 -0300

    Update index.js

--- 13/index.js
+++ 13/index.js
@@ -1,5 +1,5 @@
 function calculaArea(R,H){
-    const area = (aT = 2*3.14*R*(R+H)).toFixed(2);
+    const area = (aT = 2* Math.PI * R * (R+H)).toFixed(2);
     console.log(`A área total de um cilindro de raio ${R} e altura ${H} é de ${area}.`)
 }
 
